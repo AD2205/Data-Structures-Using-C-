2f441ac562815eefafd7e52d1f452eac6874d945
Ayush Dwivedi <talkwithayush01@gmail.com>
2022-12-16 00:34:27 +0530

    2nd commit

---
+++ absolute value.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution{
+    public:
+        int absolute(int I) {
+    if(I<0)
+        return -I;
+        else
+        return I ;
+        // Your code goes here
+        
+    }
+
+};
